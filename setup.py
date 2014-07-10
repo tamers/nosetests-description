13846ac097392bfe78b96c2891a5e441e65a9c70
Tamer Sherif <tamer.sherif@amplidata.com>
2014-07-10 10:27:08 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
+
+setup(
+    name='nose-customedescription',
+    author='Tamer Sherif',
+    author_email='me@tamer.sh',
+    version='0.1',
+    url="https://github.com/tamers/nosetests-description",
+    keywords='nose test custom description python',
+    description=('A Nose plugin to override test description output when using the'
+                 'verbose flag (-v) is used'),
+    entry_points={
+        'nose.plugins.0.10': [
+            'customdescription = custom_test_descriptor:CustomDescription',
+        ]
+    },
+    py_modules=['custom_description'],
+)
